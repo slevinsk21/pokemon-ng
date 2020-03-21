1f43e88adec7a49949ad3b917e58be5b49f7fd37
Leonel Castellano <slevinsk21@gmail.com>
2020-03-21 10:58:53 -0300

    favicon and list component number

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
-import { HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatButtonModule, MatBadgeModule, MatProgressSpinnerModule, MatCardModule, MatChipsModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PokemonListComponent
   ],
   imports: [
-    BrowserModule , AppRoutingModule , HttpClientModule, BrowserAnimationsModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+    MatBadgeModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -8,33 +8,45 @@ import { PokemonServiceService } from '../services/pokemon-service.service';
 })
 export class PokemonListComponent implements OnInit {
   list: Array<any> = [];
+  isLoading: boolean = false;
+  error: boolean = false;
   constructor(private  requester: PokemonServiceService) {
   }
 
-  async ngOnInit() {
-    const cacheList = await JSON.parse(localStorage.getItem('list'));
-    console.log(localStorage.length > 0 && cacheList);
-
-    if (localStorage.length > 0 && cacheList) {
-      this.list = cacheList
-    } else await this.listPokemon();
+  ngOnInit() {
+    this.valitadeCache();
   }
   
   async listPokemon() {
-    console.log( 'Getting Data...' );
     const params = { limit: 25 };
-    
     try {
       const response = await this.requester.getList(params);
       let data: any[] = await response['results'].map(({ url }) => this.requester.getListDetails(url));
       const responseData = await Promise.all(data);
       await localStorage.setItem('list', JSON.stringify(responseData));
       this.list = responseData;
+    } catch (error) {
+      this.error = true;
+      console.log(error);
+    }
+  }
 
+  async valitadeCache() {
+    this.isLoading = true;
+    console.log(this.isLoading);
+    try {
+      const cacheList = await JSON.parse(localStorage.getItem('list'));
+      if (localStorage.length > 0 && cacheList) {
+        this.list = cacheList
+      } else await this.listPokemon();
     } catch (error) {
       console.log(error);
-    } finally {
-      console.log( 'Getting All Data --DONE--' );
+      this.error = true;
+    }
+    finally {
+      this.isLoading = false;
+      console.log(this.isLoading);
+
     }
   }
 }
